851b95d6fbd83b9f016b09def7b4846c58a45c67
Jobin T v <jobinvarghese18@gmail.com>
2024-02-03 13:05:35 +0530

    feat: List added

--- tree-structure/src/App.tsx
+++ tree-structure/src/App.tsx
@@ -1,26 +1,84 @@
+import { useState } from "react";
 import "./App.css";
 
+interface Entry {
+  id: number;
+  name: string;
+  isOpen: boolean;
+  children?: Array<Entry>;
+}
+
+interface Props {
+  entry: Entry;
+  depth: number;
+}
+const Entry = ({ entry, depth }: Props) => {
+  console.log(entry.name, "name");
+  const [expanded, setIsExpanded] = useState(false);
+  return (
+    <div onClick={() => setIsExpanded(!expanded)}>
+      <div className="list-item" style={{ paddingLeft: `${depth * 20}px` }}>
+        {">"} {entry.name}
+      </div>
+      {entry.children?.map((entry) => {
+        return (
+          <div key={entry.id}>
+            {entry && <Entry entry={entry} depth={depth + 1} />}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 function App() {
-  const data = [
+  const [data] = useState<Array<Entry>>([
     {
+      id: 1,
       name: "nodemoduels",
-      children: [{ name: "package 1" }, { name: "package 2" }],
-    },
-    {
-      name: "src",
+      isOpen: false,
       children: [
-        { name: "components", children: [{ name: "Table" }] },
-        { name: "pages", children: [{ name: "Home" }] },
+        { id: 11, name: "package 1", isOpen: false },
+        { id: 12, name: "package 2", isOpen: false },
       ],
     },
     {
-      name: "main.tsx",
+      id: 2,
+      name: "src",
+      isOpen: false,
+      children: [
+        {
+          id: 21,
+          name: "components",
+          isOpen: false,
+          children: [{ id: 22, name: "Table", isOpen: false }],
+        },
+        {
+          id: 24,
+          name: "pages",
+          isOpen: false,
+          children: [{ id: 25, name: "Home", isOpen: false }],
+        },
+      ],
     },
     {
-      name: "index.css",
+      id: 3,
+      name: "main.tsx",
+      isOpen: false,
     },
-  ];
-  return <></>;
+    {
+      id: 4,
+      name: "index.css",
+      isOpen: false,
+    },
+  ]);
+
+  return (
+    <div>
+      {data.map((item) => {
+        return <Entry key={item.id} entry={item} depth={0} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
